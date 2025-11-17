cd4a917d264af30708d6989618e65cd090a01f5d
Puran Prasertthai <t.puran.prasertthai+scbtechx@scbtechx.io>
2025-11-17 22:49:52 +0700

    feat(test): [AIQ-114] add file for testing

--- services/data-ingestion/ingestion/test.py
+++ services/data-ingestion/ingestion/test.py
@@ -1,8 +1,16 @@
+# test.py
 from ingestion.file_reader import FileReader
+from ingestion.modality import ModalityClassifier
 
 reader = FileReader()
-results = reader.read("/Users/t.puran.prasertthai/Documents/GitHub/AINGO/services/data-ingestion/ingestion/somat.pdf")  # ← replace with your PDF file path
+info = reader.read("/Users/t.puran.prasertthai/Documents/GitHub/AINGO/services/data-ingestion/ingestion/somat.pdf")
 
-print(f"\nExtracted {len(results)} elements.\n")
-for r in results:  # just print first few
-    print(r)
+print(f"[FileReader] Detected file type: {info['file_type']}")
+
+if info["file_type"] == "pdf":
+    modality = ModalityClassifier()
+    results = modality.process_pdf(info["path"])
+    print(f"\nExtracted {len(results)} elements.\n")
+    print(results)
+else:
+    print(f"Unsupported file type: {info['file_type']}")
