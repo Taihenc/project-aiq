808cea68eab46c479189e2a7321a5479868c2338
Chawin Leardswai <t.chawin.leardswai+scbtechx@scbtechx.io>
2025-11-21 00:46:11 +0700

    feat: [AIQ-138] [AiQ-123] merge content with pre order

--- services/data-ingestion/ingestion/context_builder.py
+++ services/data-ingestion/ingestion/context_builder.py
@@ -44,10 +44,78 @@ class ContextBuilder:
 
     def __init__(self, source_id: Optional[str] = None) -> None:
         self.source_id: str = source_id or str(uuid4())
+        self.text_contents: List[ContextRecord] = []
+        self.picture_contents: List[ContextRecord] = []
+        self.table_contents: List[ContextRecord] = []
+        self.roots: List[str] = []
 
-    def build(self, page_contents: list) -> list:
+    def build(self, page_contents: list) -> List[ContextRecord]:
         """Deprecated alias for build_contexts. Use build_contexts instead."""
-        return self.text_filter_selection(page_contents) + self.picture_filter_selection(page_contents) + self.table_filter_selection(page_contents)
+        self.text_contents = self.text_filter_selection(page_contents)
+        self.picture_contents = self.picture_filter_selection(page_contents)
+        self.table_contents = self.table_filter_selection(page_contents)
+        return self.merge_contents()
+
+
+    def format_reference(self, raw: str) -> str:
+        parts = raw.strip("#/").split("/")   # remove "#/" then split
+
+        if len(parts) != 2:
+            return None
+
+        key, idx = parts[0], int(parts[1])
+        
+        if key == "texts":
+            return self.text_contents[idx]["id"]
+        if key == "pictures":
+            return self.picture_contents[idx]["id"]
+        if key == "tables":
+            return self.table_contents[idx]["id"]
+
+    def merge_contents(self) -> List[ContextRecord]:
+
+        contents: Dict[str, ContextRecord] = {}
+        for sections in [self.text_contents, self.picture_contents, self.table_contents]:
+            for content in sections:
+                raw_children = content["metadata"]["children"]
+                formatted_children = [self.format_reference(child) for child in raw_children]
+
+                content["metadata"]["children"] = formatted_children
+
+                if content["metadata"]["parent"] == "#/body":
+                    self.roots.append(content["id"])
+                    content["metadata"]["parent"] = None
+                else:
+                    content["metadata"]["parent"] = self.format_reference(content["metadata"]["parent"])
+                
+                contents[content["id"]] = content
+
+        merge_contents: List[ContextRecord] = []
+
+        ### Simple Merge ###
+
+        text = []
+        for root in self.roots:
+            self.pre_order_merge(contents[root], text, contents)
+            
+        merge_content: ContextRecord = {
+            "id": "",
+            "text": " ".join(text),
+            "metadata": {
+                "file": self.text_contents[0]["metadata"]["file"] or self.picture_contents[0]["metadata"]["file"] or self.table_contents[0]["metadata"]["file"],
+            }
+        }
+        merge_contents.append(merge_content)
+        ####################
+
+        return merge_contents
+
+    def pre_order_merge(self, content: ContextRecord, text, contents: Dict[str, ContextRecord]):
+        text.append(content["text"])
+
+        for child in content["metadata"]["children"]:
+            self.pre_order_merge(contents[child], text, contents)
+
 
     def safe_get(self, data, *keys, default=None, pnt=False):
         """
@@ -125,9 +193,6 @@ class ContextBuilder:
             raw_text = text.get("text", "") or ""
             cleaned_text = clean_text(raw_text)
 
-            if not cleaned_text:
-                continue
-
             file = self.safe_get(page_contents, "origin", "filename", default="")
             page = self.safe_get(text, "prov", 0, "page_no", default=0)
             label = text.get("label", "")
@@ -141,8 +206,10 @@ class ContextBuilder:
                 "source_id": self.source_id,
                 "checksum": sha1_bytes(cleaned_text.encode("utf-8")),
                 "created_at": utc_now_iso(),
-                "parent": "",
-                "children": [],
+                "parent": self.safe_get(text, "parent", "$ref", default=""),
+                "children": [
+                    self.safe_get(child, "$ref", default="") for child in self.safe_get(text, "children", default="")
+                ],
             }
 
             record: ContextRecord = {
@@ -165,9 +232,6 @@ class ContextBuilder:
             raw_text = "".join([a.get("text", "") for a in annotations])
             cleaned_text = clean_text(raw_text)
 
-            if not cleaned_text:
-                continue
-
             file = self.safe_get(page_contents, "origin", "filename", default="")
             page = self.safe_get(pic, "prov", 0, "page_no", default=0)
             label = pic.get("label", "")
@@ -181,8 +245,10 @@ class ContextBuilder:
                 "source_id": self.source_id,
                 "checksum": sha1_bytes(cleaned_text.encode("utf-8")),
                 "created_at": utc_now_iso(),
-                "parent": "",
-                "children": [],
+                "parent": self.safe_get(pic, "parent", "$ref", default=""),
+                "children": [
+                    self.safe_get(child, "$ref", default="") for child in self.safe_get(pic, "children", default="")
+                ],
             }
 
             record: ContextRecord = {
@@ -205,9 +271,6 @@ class ContextBuilder:
             raw_text = self.table_extractor(desc_text)
             cleaned_text = clean_text(raw_text)
 
-            if not cleaned_text:
-                continue
-
             file = self.safe_get(page_contents, "origin", "filename", default="")
             page = self.safe_get(table, "prov", 0, "page_no", default=0)
             label = table.get("label", "")
@@ -221,8 +284,10 @@ class ContextBuilder:
                 "source_id": self.source_id,
                 "checksum": sha1_bytes(cleaned_text.encode("utf-8")),
                 "created_at": utc_now_iso(),
-                "parent": "",
-                "children": [],
+                "parent": self.safe_get(table, "parent", "$ref", default=""),
+                "children": [
+                    self.safe_get(child, "$ref", default="") for child in self.safe_get(table, "children", default="")
+                ],
             }
 
             record: ContextRecord = {
